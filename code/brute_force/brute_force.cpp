12e0f130b6b4d865862b21244437d27eaca2d772
vintroll <87960386+vintroll@users.noreply.github.com>
2025-06-03 19:41:26 -0400

    Update brute_force.cpp

--- code/brute_force/brute_force.cpp
+++ code/brute_force/brute_force.cpp
@@ -1,59 +1,84 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <fstream>
+
+
+
 
 using namespace std;
+using namespace chrono;
+// Fuerza bruta para calcular la LCS (sin memoización)
+string calcular_lcs_brute(const string &s, const string &t) {
+    if (s.empty() || t.empty())
+        return "";
 
-vector<pair<string, string>> comparar_por_fuerza_bruta(const string &s, const string &t) {
-    vector<pair<string, string>> resultado;
-    int i = 0, j = 0;
-    int n = s.size(), m = t.size();
-
-    while (i < n || j < m) {
-        if (i < n && j < m && s[i] == t[j]) {
-            ++i;
-            ++j;
-        } else {
-            int ini_i = i, ini_j = j;
-            while (i < n && (j >= m || s[i] != t[j])) ++i;
-            while (j < m && (i >= n || s[i] != t[j])) ++j;
-
-            string a = s.substr(ini_i, i - ini_i);
-            string b = t.substr(ini_j, j - ini_j);
-            resultado.emplace_back(a, b);
-        }
+    if (s[0] == t[0])
+        return s[0] + calcular_lcs_brute(s.substr(1), t.substr(1));
+    else {
+        string lcs1 = calcular_lcs_brute(s.substr(1), t);
+        string lcs2 = calcular_lcs_brute(s, t.substr(1));
+        return (lcs1.size() > lcs2.size()) ? lcs1 : lcs2;
     }
+}
+
+vector<pair<string, string>> diferencias_por_lcs(const string &s, const string &t) {
+    string lcs = calcular_lcs_brute(s, t);
+    vector<pair<string, string>> resultado;
+    int i = 0, j = 0, k = 0;
+
+    while (k < lcs.size()) {
+        string a, b;
+        while (i < s.size() && s[i] != lcs[k]) a += s[i++];
+        while (j < t.size() && t[j] != lcs[k]) b += t[j++];
+        if (!a.empty() || !b.empty()) resultado.emplace_back(a, b);
+        ++i; ++j; ++k;
+    }
+
+    string a_rest = s.substr(i);
+    string b_rest = t.substr(j);
+    if (!a_rest.empty() || !b_rest.empty())
+        resultado.emplace_back(a_rest, b_rest);
 
     return resultado;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ifstream input("brute_force_input/input.txt");
+    ofstream output("measurements/brute_force/output.txt");
+    ofstream timing("measurements/brute_force/time.txt");
 
     int K;
-    cin >> K;
-    cout << K << '\n';
-    cin.ignore();
+    input >> K;
+    output << K << '\n';
+    input.ignore();
+
+    auto start = high_resolution_clock::now();
 
     while (K--) {
         string s_line, t_line, s, t;
         int n, m;
 
-        getline(cin, s_line);
-        getline(cin, t_line);
+        getline(input, s_line);
+        getline(input, t_line);
 
         n = stoi(s_line.substr(0, s_line.find(' ')));
         s = s_line.substr(s_line.find(' ') + 1);
         m = stoi(t_line.substr(0, t_line.find(' ')));
         t = t_line.substr(t_line.find(' ') + 1);
 
-        vector<pair<string, string>> diferencias = comparar_por_fuerza_bruta(s, t);
-        cout << diferencias.size() << '\n';
+        vector<pair<string, string>> diferencias = diferencias_por_lcs(s, t);
+
+        output << diferencias.size() << '\n';
         for (auto &[a, b] : diferencias) {
-            cout << a << ' ' << b << '\n';
+            output << a << ' ' << b << '\n';
         }
     }
+
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    timing << "Tiempo de ejecución (ms): " << duration.count() << endl;
 
     return 0;
 }
